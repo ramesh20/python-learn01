c5116482e2f381e47b691729f06a92ba7647e8d3
Sushma Acharya <sushmaacharya345@gmail.com>
2024-03-07 18:05:52 +0545

    Add the folder and main.py files

---
+++ lesson_09/main.py
@@ -0,0 +1,41 @@
+# string 
+# check any type 
+# a = "ramesn" type(a) / a.lower() / a.upper() | a.title() -> capatalise \n -> line break
+# len(a) length check / a[0] -> r
+
+first_name = "Ramesh"
+
+if( first_name.isalpha() ):
+    print("Your name is: " + first_name)
+else:
+    print("Invalid letter")
+
+
+# input with string
+input_string = str("Ramesh Gautam")
+# input_string = str(input("Enter the desired string: "))
+list_data = list(input_string)
+print(list_data)
+print(str(list_data))
+
+# tuples -> data types, store the data
+# [] list -> [1, 2, 3, 4, 5]
+# () tuple (1, 2, 3, 4, 5) -> index(lowest index indeing), count(repeat value count)
+
+first_data = [1, 2, 3, 4, 5]
+second_data = (1, 2, 3, 4, 5)
+third_data = {1, 2, 3, 4, 5}
+
+type_list = list(second_data)
+type_list.pop([2])
+# del type_list[2]  
+
+print(type(second_data))
+
+print(len(second_data))
+
+print(second_data[0])
+print(second_data[-1])
+
+for numbers in second_data:
+    print(numbers)
--- lesson_condition08/conditions.py
+++ lesson_condition08/conditions.py
@@ -16,12 +16,29 @@ else:
 print("block end")
 '''
 
-val_first = input("Enter the first value: ")
-val_second = input("Enter the second value: ")
+# val_first = input("Enter the first value: ")
+# val_second = input("Enter the second value: ")
 
-if ( val_first > val_second ):
-	print(f'the first value {val_first} is grater then second value {val_second}')
-elif ( val_first == val_second ):
-	print(f'the first value {val_first} is equal to the second value {val_second}')
-else: 
-	print(f'the second value {val_second} is grater then first value {val_first}')
+# if ( val_first > val_second ):
+# 	print(f'the first value {val_first} is grater then second value {val_second}')
+# elif ( val_first == val_second ):
+# 	print(f'the first value {val_first} is equal to the second value {val_second}')
+# else: 
+# 	print(f'the second value {val_second} is grater then first value {val_first}')
+
+input_total_number = int(input("Enter your Total SLC number (marks): "))
+# input_percentage = input("Enter your SLC percentage: ")
+input_percentage = int(input_total_number/8)
+
+if( input_percentage > 100 ):
+	print("Seems your score is higher than the total score !")
+elif ( input_percentage >= 80 and  input_percentage <= 100 ):
+	print("Seems your score is Distinction !")
+elif ( input_percentage >= 60 and input_percentage < 45 ):
+	print("Seems your score is first division !")
+elif ( input_percentage >= 45 and input_percentage < 35 ):
+	print("Seems your score is second division !")
+elif ( input_percentage >= 35 ):
+	print("Seems your score is second division !")
+else:
+	print("Seems your score is not enough, try again!")
